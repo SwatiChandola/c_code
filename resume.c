3e9d2fb1c0d9d4dcebd4570bd0faa87e252527d9
SwatiChandola <101933216+SwatiChandola@users.noreply.github.com>
2022-03-20 21:16:21 +0530

    Update resume.c

--- resume.c
+++ resume.c
@@ -2,9 +2,9 @@
 #include<conio.h>
 void main()
 {
-	printf("\n NAME:-Siddhant Gulati.");
-	printf("\n ADDRESS:-I-7/2,sec 16 rohini, delhi");
+	printf("\n NAME:-Swati Chandola.");
+	printf("\n ADDRESS:- house no 330-331,sec 8 E 16 rohini, delhi");
 	printf("\n EDUCATION:- Pursuing Bachelor of Computer Applications");
-	printf("\n MOBILE:- +91 9911XXXXXX \n\n");
+	printf("\n MOBILE:- +91 9966XXXXXX \n\n");
     getch();
 }
