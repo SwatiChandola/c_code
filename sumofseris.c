b5c36466888ba793af101cbaa0acd02bf2c5893d
SwatiChandola <101933216+SwatiChandola@users.noreply.github.com>
2022-03-20 21:31:22 +0530

    Update sumofseris.c

--- sumofseris.c
+++ sumofseris.c
@@ -2,7 +2,7 @@
 int main()
 {
    int i,n,sum=0;
-   printf("siddhant\n");
+   printf("swati\n");
    printf("Input Value of terms : ");
    scanf("%d",&n);
    printf("\nThe first %d natural numbers are:\n",n);
